9058c6c01c3daef6487a563791c3de859e730fda
flare-s <webflarez@gmail.com>
2022-06-19 22:03:25 +0100

    Refactored code and added new tests

--- assertEquals.js
+++ assertEquals.js
@@ -1,62 +1,88 @@
+// Check if two values are strictly equal
 function assertEquals(expected, actual) {
     if (expected === actual) {
         return true;
     } else {
-        if (expected == actual) {
-            throw new Error(`Expected the type to be ${typeof expected} but found ${typeof actual}`);
-        } else if(Array.isArray(expected)) {
-            checkArrayAgainstValue(expected, actual);
+         if(Array.isArray(expected)) {
+            return checkArrayAgainstValue(expected, actual);
+        } else if(typeof expected === "string" && typeof actual === "string"){
+            return checkStringCaseType(expected, actual);
         } else {
-            checkUnequalValues(expected, actual)
+            return checkDifferentTypes(expected, actual);
         }
     }
 }
 
-
-const checkUnequalValues = (value1, value2) => {
-    if (typeof value1 === "string") {
-        if (typeof value2 === "string") {
-            throw new Error(`Expected "${value1}" but found "${value2}"`);
-        }
+// Check if two strings on the lower case type are equal
+const checkStringCaseType = (string1, string2) => {
+    if (string1.toLowerCase() === string2.toLowerCase()) {
+        throw new Error(`Expected "${string1}" but found "${string2}". Please check the string type case.`);
     } else {
-        if (typeof value2 === "string") {
-            throw new Error(`Expected ${value1} but found "${value2}"`);
-        } else {
-            throw new Error(`Expected ${value1} but found ${value2}`);
-        }
+        throw new Error(`Expected "${string1}" but found "${string2}"`);
     }
 }
 
+// Check if two arrays are identical
+const areArraysEqual = (arr1, arr2) =>{
+    return arr1.every((el, ind) => el === arr2[ind])
+}
+
+// Check if two arrays are equal or not
 const checkArraysEquality = (arr1, arr2) => {
-    if (!arr1.every((el, ind) => el === arr2[ind])) {
-        arr1.forEach((el, ind) => {
+    if (!areArraysEqual(arr1, arr2)) {
+        return arr1.forEach((el, ind) => {
             if (el !== arr2[ind]) {
-                if (typeof el === "string") {
-                    if (typeof arr2[ind] === "string") {
-                        throw new Error(`Expected "${el}" but found "${arr2[ind]}"`);
-                    } else {
-                        throw new Error(`Expected "${el}" but found ${arr2[ind]}`);
-                    }
+                if(typeof el === "string" && typeof arr2[ind] === "string"){
+                    return checkStringCaseType(el, arr2[ind]);
                 } else {
-                    throw new Error(`Expected ${el} but found ${arr2[ind]}`);
+                    return checkDifferentTypes(el, arr2[ind]);
                 }
             }
         });
-    } else {
-        return true;
     }
+    return true;
 }
 
+// Check an array aganst a value.
+/**
+ * Check if the second value is an array.
+ * If not throw the appropriate error.
+ * If it's an array, check if the length of both arrays equal.
+ * If not, throw an error.
+ * If it is, check both arrays to see if they are equal.
+ */
 const checkArrayAgainstValue = (arr, value) => {
     if (Array.isArray(value)) {
         if (arr.length === value.length) {
-            checkArraysEquality(arr, value);
+            return checkArraysEquality(arr, value);
         } else {
-            throw new Error(`Expected the length to be ${arr.length} but found ${value.length}`);
+            throw new Error(`Expected the array length to be ${arr.length} but found ${value.length}`);
         }
     } else {
         throw new Error(`Expected an array but found ${typeof value}`)
     }
+}
+
+// Check if either values is null
+const isNull = (value1, value2) => {
+    if (value1 === null) {
+        throw new Error(`Expected type null but found ${typeof value2}`);
+    } else if (value2 === null) {
+        throw new Error(`Expected type ${typeof value1} but found null`);
+    }
+}
+
+// Check different types and throw the appropriate error
+const checkDifferentTypes = (value1, value2) => {
+    if(Array.isArray(value2)) {
+        throw new Error(`Expected type ${typeof value1} but found array`);
+    }
+    isNull(value1, value2);
+
+    if (typeof value1 !== typeof value2) {
+        throw new Error(`Expected type ${typeof value1} but found ${typeof value2}`);
+    }
+    throw new Error(`Expected ${value1} but found ${value2}`);
 }
 
 module.exports = assertEquals;
--- assertEquals.spec.js
+++ assertEquals.spec.js
@@ -1,30 +1,98 @@
 const assertEquals = require('./assertEquals')
 
 describe('assertEquals', () => {
+  // Strings test
   describe('when expected and actual are the same string', () => {
     it('returns without throwing an error', () => {
-      expect(() => assertEquals('abc', 'abc')).not.toThrow()
+      expect(() => typeof assertEquals('abc', 'abc') === "boolean").toBeTruthy();
     })
   })
 
   describe('when expected and actual are different strings', () => {
     it('throws an error', () => {
-      expect(() => assertEquals("abc", "acb")).toThrow(`Expected "abc" but found "acb"`)
+      expect(() => assertEquals("abc", "acb")).toThrow(`Expected "abc" but found "acb"`);
     })
   })
+
+  describe('when expected and actual both are strings with the same value but different case type', () => {
+    it('throws an error', () => {
+      expect(() => assertEquals("ABC", "abc")).toThrow(`Expected "ABC" but found "abc". Please check the string type case.`);
+    })
+  })
+
+  describe('when expected and actual both are different types', () => {
+    it('throws an error', () => {
+      expect(() => assertEquals("ABC", undefined)).toThrow(`Expected type string but found undefined`);
+    })
+  })
+
+  // Numbers test
+  describe('when expected and actual are the same number', () => {
+    it('returns without throwing an error', () => {
+      expect(() => typeof assertEquals(1, 1) === "boolean").toBeTruthy()
+    })
+  })
+
+  describe('when expected and actual are different numbers', () => {
+    it('throws an error', () => {
+      expect(() => assertEquals(1, 2)).toThrow(`Expected 1 but found 2`);
+    })
+  })
+
+  describe('when expected and actual both are different types', () => {
+    it('throws an error', () => {
+      expect(() => assertEquals(1, true)).toThrow(`Expected type number but found boolean`);
+    })
+  })
+
+  // Arrays test
+  describe('when expected array and actual array have the same elements', () => {
+    it('Returns true without throwing an error', () => {
+      expect(() => typeof assertEquals(["abc", 1, true], ["abc", 1, true]) === "boolean").toBeTruthy();
+    })
+  })
+
+  describe('when expected array and actual have different lengths', () => {
+    it('throws an error', () => {
+      expect(() => assertEquals(["abc", 1], ["abc"])).toThrow(`Expected the array length to be 2 but found 1`)
+    })
+  })
+
+  describe('when expected and actual arrays have the same length but different elements', () => {
+    it('throws an error', () => {
+      expect(() => assertEquals([1, "abc", 3], [1, "acb", 3])).toThrow(`Expected "abc" but found "acb"`)
+    })
+  })
+
+  describe('when expected and actual both are different types', () => {
+    it('throws an error', () => {
+      expect(() => assertEquals([1, "abc", 3], false)).toThrow(`Expected an array but found boolean`)
+    })
+  })
+
+  describe('when expected and actual have a string with the same value but different case type', () => {
+    it('throws an error', () => {
+      expect(() => assertEquals([1, "abc", 3], [1, "AbC", 3])).toThrow(`Expected "abc" but found "AbC". Please check the string type case.`)
+    })
+  })
+
+  // Other Different types
+  describe('when expected and actual are of dfferent types', () => {
+    it('throws an error', () => {
+      expect(() => assertEquals({1: "1"}, null)).toThrow(`Expected type object but found null`)
+    })
+  })
+
   describe('when expected and actual are the same value but different types', () => {
     it('throws an error', () => {
-      expect(() => assertEquals("abc", "acb")).toThrow(`Expected "abc" but found "acb"`)
+      expect(() => assertEquals(null, undefined)).toThrow(`Expected type null but found undefined`)
     })
   })
-  describe('when expected and actual =have different lengths', () => {
+
+  describe('when expected and actual are the same value but different types', () => {
     it('throws an error', () => {
-      expect(() => assertEquals("abc", "acb")).toThrow(`Expected "abc" but found "acb"`)
+      expect(() => assertEquals({}, [])).toThrow(`Expected type object but found array`)
     })
   })
-  describe('when expected and actual have the same length but different elements', () => {
-    it('throws an error', () => {
-      expect(() => assertEquals("abc", "acb")).toThrow(`Expected "abc" but found "acb"`)
-    })
-  })
+
 })
